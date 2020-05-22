dcc79eed569f80de9868fe3c9bd065e8ca3a6109
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-05-22 00:42:05 -0300

    .

--- js/tabela.js
+++ js/tabela.js
@@ -1,16 +1,11 @@
 var intervalo = 2000;
 
 function slide1(){
-    document.getElementById('foto').src = './img/Lol.jpg'
+    document.getElementById('foto').src = './img/unnamed.jpg'
     setTimeout("slide2()", intervalo)
 }
 
 function slide2(){
-    document.getElementById('foto').src = './img/unnamed.jpg'
-    setTimeout("slide3()", intervalo)
-}
-
-function slide3(){
     document.getElementById('foto').src = './img/camp.jpg'
     setTimeout("slide1()", intervalo)
 }
